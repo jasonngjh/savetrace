2f6d1fcbe76702ed04e44fc329cbea7e34e69353
jasonngjh <13460291+jasonngjh@users.noreply.github.com>
2021-02-18 15:58:53 +0800

    added test data

--- database/seeders/DoctorSeeder.php
+++ database/seeders/DoctorSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Doctor;
+use App\Models\Patient;
 use App\Models\PracticePlace;
 use App\Models\Nurse;
 use App\Models\User;
@@ -18,128 +19,127 @@ class DoctorSeeder extends Seeder
      */
     public function run()
     {
-        // $output = new \Symfony\Component\Console\Output\ConsoleOutput();
-        // $practice_place_data = array();
-        // $doctors_data = array();
-        // $nurses_data = array();
-        // $users_data = array();
-        // $patients_data = array();
+        $practice_place_data = array();
+        $doctors_data = array();
+        $nurses_data = array();
+        $users_data = array();
+        $patients_data = array();
 
-        // if (($handle = fopen("/Users/jasonng/Desktop/drive-download-20210218T053958Z-001/practiceplaces.csv", "r")) !== FALSE) {
-        //     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        //         $practice_place_data[] = $data;
-        //     }
-        //     fclose($handle);
-        // }
+        if (($handle = fopen("database/testdata/practiceplaces.csv", "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $practice_place_data[] = $data;
+            }
+            fclose($handle);
+        }
 
-        // if (($handle = fopen("/Users/jasonng/Desktop/drive-download-20210218T053958Z-001/nurses.csv", "r")) !== FALSE) {
-        //     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        //         $nurses_data[] = $data;
-        //     }
-        //     fclose($handle);
-        // }
+        if (($handle = fopen("/database/testdata/nurses.csv", "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $nurses_data[] = $data;
+            }
+            fclose($handle);
+        }
 
-        // if (($handle = fopen("/Users/jasonng/Desktop/drive-download-20210218T053958Z-001/doctors.csv", "r")) !== FALSE) {
-        //     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        //         $doctors_data[] = $data;
-        //     }
-        //     fclose($handle);
-        // }
+        if (($handle = fopen("/database/testdata/doctors.csv", "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $doctors_data[] = $data;
+            }
+            fclose($handle);
+        }
 
-        // if (($handle = fopen("/Users/jasonng/Desktop/drive-download-20210218T053958Z-001/users.csv", "r")) !== FALSE) {
-        //     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        //         $users_data[] = $data;
-        //     }
-        //     fclose($handle);
-        // }
+        if (($handle = fopen("database/testdata/users.csv", "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $users_data[] = $data;
+            }
+            fclose($handle);
+        }
 
-        // $pp_count = 0;
-        // foreach ($practice_place_data as $pratice_place) {
-        //     if ($pp_count == 0) {
-        //         $pp_count++;
-        //         continue;
-        //     }
-        //     PracticePlace::create([
-        //         'name' => $pratice_place[3],
-        //         'address' => $pratice_place[4],
-        //         'tel' => $pratice_place[5],
-        //         'area' => $pratice_place[6],
-        //         'opening_time' => $pratice_place[7],
-        //     ]);
-        // }
+        if (($handle = fopen("database/testdata/patients.csv", "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $patients_data[] = $data;
+            }
+            fclose($handle);
+        }
 
-        // $user_header = 0;
-        // foreach ($users_data as $user) {
-        //     if ($user_header == 0) {
-        //         $user_header++;
-        //         continue;
-        //     }
+        $pp_count = 0;
+        foreach ($practice_place_data as $pratice_place) {
+            if ($pp_count == 0) {
+                $pp_count++;
+                continue;
+            }
+            PracticePlace::create([
+                'name' => $pratice_place[3],
+                'address' => $pratice_place[4],
+                'tel' => $pratice_place[5],
+                'area' => $pratice_place[6],
+                'opening_time' => $pratice_place[7],
+            ]);
+        }
 
-        //     $thisuser = User::create([
-        //         'name' => $user[1],
-        //         'email' => $user[2],
-        //         'password' => Hash::make('password'),
-        //         'contact_number' => $user[8],
-        //     ]);
+        $user_header = 0;
+        foreach ($users_data as $user) {
+            if ($user_header == 0) {
+                $user_header++;
+                continue;
+            }
 
-        //     if ($user[13] == 'internal') {
-        //         foreach ($doctors_data as $doctor) {
-        //             if ($user[1] == $doctor[3]) {
-        //             }
-        //         }
-        //     }
-        // }
+            $thisuser = User::create([
+                'name' => $user[1],
+                'email' => $user[2],
+                'password' => Hash::make('password'),
+                'contact_number' => $user[8],
+            ]);
 
-        $practice = PracticePlace::create([
-            'name' => 'Raffles Hospital',
-            'address' => '585 North Bridge Road, Raffles Hospital, Singapore 188770',
-            'tel' => '63111111',
-            'created_at' => now(),
-            'area' => 'C',
-            'opening_time' => 'Mon - Fri: 8:30 a.m. - 12:30 p.m.
-            1:30 p.m. - 5 p.m.
-            Sat: 8:30 a.m. - 12:30 p.m.',
-        ]);
+            if ($user[13] == 'internal' or $user[13] == 'external') {
+                foreach ($doctors_data as $doctor) {
+                    if ($user[1] == $doctor[3]) {
+                        $thisdoctor = Doctor::create([
+                            'name' => $doctor[3],
+                            'registration_number' => $doctor[4],
+                            'internal' => $user[13] == 'internal' ? True : False,
+                            'email' => $doctor[5],
+                            'contact' => $doctor[6],
+                            'fax' => $doctor[7],
+                            'specialty' => $doctor[9],
+                            'information' => $doctor[11],
+                            'practice_place' => (PracticePlace::select('id')->where('name', '=', $doctor[12])->get())->first()->id,
+                            'user_id' => $thisuser->id,
+                        ]);
 
-        $practice2 = PracticePlace::create([
-            'name' => 'Ng Teng Fong General Hospital',
-            'address' => '1 Jurong East Street 21 Singapore 609606',
-            'tel' => '68872020',
-            'created_at' => now(),
-            'area' => 'W',
-            'opening_time' => 'Mon - Fri: 8:30 a.m. - 12:30 p.m.
-            1:30 p.m. - 5 p.m.
-            Sat: 8:30 a.m. - 12:30 p.m.',
-        ]);
+                        $thisuser->role_id = $thisdoctor->id;
+                        $thisuser->save();
+                    }
+                }
+            } elseif ($user[13] == 'nurse') {
+                foreach ($nurses_data as $nurse) {
+                    if ($nurse[4] == $user[0]) {
+                        $thisnurse = Nurse::create([
+                            'practice_place' => (PracticePlace::select('id')->where('name', '=', $nurse[3])->get())->first()->id,
+                            'user_id' => $thisuser->id,
+                        ]);
 
-        $internalDocWithId = Doctor::create([
-            'name' => 'Tan Eik Hock Andrew',
-            'registration_number' => 'M10086B',
-            'email' => 'andrew@raffleshospital.com',
-            'contact' => '63111102',
-            'internal' => true,
-            'specialty' => 'Nuclear Medicine',
-            'practice_place' => $practice->id,
-            'created_at' => now(),
-            'user_id' => 2,
-        ]);
+                        $thisuser->role_id = $thisnurse->id;
+                        $thisuser->save();
+                    }
+                }
+            } else {
+                foreach ($patients_data as $patient) {
+                    if ($patient[4] == $user[1]) {
+                        $thispatient = Patient::create([
+                            'user_id' => $thisuser->id,
+                            'name' => $patient[4],
+                            'date_of_birth' => date_format(date_create_from_format('d/m/Y', $patient[5]), 'Y-m-d'),
+                            'contact_number' => $patient[6],
+                            'name_of_emergency_contact' => $patient[7],
+                            'contact_number_of_emergency_contact' => $patient[8],
+                            'address' => $patient[9],
+                            'allergies' => $patient[11],
+                        ]);
 
-        $externalDocWithId = Doctor::create([
-            'name' => 'Ajeet Madhav Wagle',
-            'registration_number' => 'M10797B',
-            'internal' => false,
-            'email' => 'ajeetwagle@gmail.com',
-            'contact' => '6443 2020',
-            'specialty' => 'Ophthalmology',
-            'practice_place' => $practice2->id,
-            'created_at' => now(),
-            'user_id' => 3,
-            'information' => 'Dr. Ajeet Madhav Wagle is a Medical Director and Senior Consultant at International Eye Cataract Retina Centre at Farrer Park Medical Centre. He specializes in the medical and surgical management of retinal diseases such as retinal detachment, macular degeneration, diabetic retinopathy, retinal vascular occlusions as well as advanced modern cataract surgery and comprehensive eye care.',
-        ]);
-
-        $employee = Nurse::create([
-            'user_id' => 4,
-            'practice_place' => $practice->id,
-        ]);
+                        $thisuser->role_id = $thispatient->id;
+                        $thisuser->save();
+                    }
+                }
+            }
+        }
     }
 }
--- database/seeders/RolesAndPermissionSeeder.php
+++ database/seeders/RolesAndPermissionSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use App\Models\User;
-use App\Models\Team;
 use Illuminate\Support\Facades\Hash;
 
 class RolesAndPermissionSeeder extends Seeder
@@ -24,9 +23,9 @@ class RolesAndPermissionSeeder extends Seeder
         //Permission::create(['name' => '']);
 
         $adminRole = Role::create(['name' => 'system admin']);
-        $internalDocRole = Role::create(['name' => 'internal']);
-        $externalDocRole = Role::create(['name' => 'external']);
-        $employeeRole = Role::create(['name' => 'nurse']);
+        Role::create(['name' => 'internal']);
+        Role::create(['name' => 'external']);
+        Role::create(['name' => 'nurse']);
         Role::create(['name' => 'patient']);
 
         $admin = User::factory()->create([
@@ -37,35 +36,5 @@ class RolesAndPermissionSeeder extends Seeder
         ]);
 
         $admin->assignRole($adminRole);
-
-        $internalDoc = User::factory()->create([
-            'name' => 'internal doctor',
-            'email' => 'internalDoc@savetrace.com',
-            'password' => Hash::make('password'),
-            'contact_number' => '88732321',
-            'role_id' => 1,
-        ]);
-
-        $internalDoc->assignRole($internalDocRole);
-
-        $externalDoc = User::factory()->create([
-            'name' => 'external doctor',
-            'email' => 'externalDoc@externalclinic.com',
-            'password' => Hash::make('password'),
-            'contact_number' => '88732321',
-            'role_id' => 2,
-        ]);
-
-        $externalDoc->assignRole($externalDocRole);
-
-        $employee = User::factory()->create([
-            'name' => 'employee',
-            'email' => 'employee@savetrace.com',
-            'password' => Hash::make('password'),
-            'contact_number' => '98458321',
-            'role_id' => 1,
-        ]);
-
-        $employee->assignRole($employeeRole);
     }
 }
